c1cc9366c8e2039842907da7fc2c55bfe62c1a29
Lisa-cai <145958405+Lisa-cai@users.noreply.github.com>
2024-01-05 21:14:08 -0800

    Add project files.

---
+++ GA_Class_Hongfei/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_Class_Hongfei
+{
+    internal class Program
+    {
+        //Hongfei 
+        //Assignment: Create class
+        //01/05/2024
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello, World");
+
+            Student newStudent = new Student();
+            Student Lisa = new Student();
+            Professor ProLisa = new Professor();
+            Pet newPet = new Pet();
+            Pet cat = new Pet();
+        }
+    }
+}
